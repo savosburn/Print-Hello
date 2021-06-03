4c92592cd63b7928d0339a6eb019ddaed337cf95
savosburn <savosburn18@tampabay.rr.com>
2021-06-03 12:30:27 -0400

    Initial commit

---
+++ src/main/java/ex01/App.java
@@ -0,0 +1,19 @@
+package ex01;
+
+/*
+Exercise 1 - Saying Hello
+
+Create a program that prompts for your name and prints a greeting using your name.
+
+Example Output
+
+What is your name? Brian
+Hello, Brian, nice to meet you!
+
+Constraint
+Keep the input, string concatenation, and output separate.
+ */
+
+public class App {
+
+}
